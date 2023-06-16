1443af60093a7ce3f3d05ac860bd354832264465
Nineseven <nine@nineseven.ru>
2023-06-16 18:14:00 +0300

    Updates from linux

--- helpers/autoloader.php
+++ helpers/autoloader.php
@@ -7,10 +7,17 @@ class Autoloader
 {
     public static function register()
     {
-       spl_autoload_register(function ($class) {
-            $file = '../' . str_replace('\\', DIRECTORY_SEPARATOR, $class) . '.php';
+        spl_autoload_register(function ($class) {
+            $parts = explode('\\', $class); // Разбиваем строку по символу обратного слэша ("\")
+            $className = end($parts); // Получаем последний элемент массива
 
-            
+            $className = lcfirst($className); // Заменяем заглавную первую букву на строчную
+
+            // Возвращаем обработанную строку в переменную $class
+            $class = implode('\\', array_slice($parts, 0, -1)) . '\\' . $className;
+            $file = '..' . DIRECTORY_SEPARATOR . str_replace('\\', DIRECTORY_SEPARATOR, $class) . '.php';
+           
+
             if (file_exists($file)) {
                 require  $file;
 
@@ -22,4 +29,3 @@ class Autoloader
     }
 }
 Autoloader::register();
-
